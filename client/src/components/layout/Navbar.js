c8cb1e03f7ed84e4c8e41efc613d495062254772
pxuanbach <pxuanbach1412@gmail.com>
2022-03-10 19:50:25 +0700

    Create Navbar.js

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,25 @@
+import React, {useContext} from 'react'
+import { UserContext } from '../../UserContext'
+import './Navbar.css'
+import SignedInMenu from './SignedInMenu'
+import SignedOutMenu from './SignedOutMenu'
+
+const Navbar = () => {
+    const {user, setUser} = useContext(UserContext);
+
+    const logout = () => {
+
+    }
+
+    const menu = user ? <SignedInMenu logout={logout}/> : <SignedOutMenu/>
+    return (
+        <nav>
+            <div className="nav-wrapper">
+                <a href="#" className="brand-logo">Logo</a>
+                <SignedInMenu logout={logout}/>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar 
