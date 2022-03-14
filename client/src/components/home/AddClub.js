1410383381c3b978d0def299b372a6a72fe82d2d
tiendat19521345 <dat31122001@gmail.com>
2022-03-14 20:06:29 +0700

    update-form-home

---
+++ client/src/components/home/AddClub.js
@@ -0,0 +1,57 @@
+import React, {useState,useRef} from 'react'
+import "./AddClub.css"
+import Avatar from '@mui/material/Avatar';
+import AvatarGroup from '@mui/material/AvatarGroup';
+const AddClub = ({setShowFormAddClub})=>{
+    const onExitClick = () => {
+        setShowFormAddClub(false);
+      };
+    return(
+        <div className='div-add'>
+            <div className='div-info'>
+                <div className='title'>
+                    Tạo câu lạc bộ mới
+                    <p>Cộng tác chặt chẽ với một nhóm người trong tổ chức của bạn dựa trên dự án, sáng kiến hoặc lợi ích chung.</p>
+                </div>
+                <div className='info'>
+                    <div className='div-left'>
+                        <div className='div-image'>
+                            <button className='btn-image'>Chọn ảnh CLB</button>
+                            {/* <label className='label1'>Ảnh minh họa</label> */}
+                        </div>
+                    </div>
+                    <div className='div-right'>
+                        <div className='div-team-name'>
+                            <label className='label'>Tên câu lạc bộ</label>
+                            <input className='input-name'></input>
+                        </div>
+                        <div className='div-description'>
+                            <label className='label'>Mô tả</label>
+                            <textarea className='desciption' cols="100" rows="4" placeholder="Vui lòng nhập tại đây..."></textarea>
+                        </div>
+                    </div>
+                </div>
+                <div className='div-team-search'>
+                    <label className='label-search'>Thêm thành viên</label>
+                    <i class="fa-solid fa-user-plus"></i>
+                    <input className='input-search' placeholder="Nhập tên thành viên..."></input>
+                    <AvatarGroup className='avatagroup' total={24}>
+                        <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                        <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+                        <Avatar alt="Agnes Walker" src="/static/images/avatar/4.jpg" />
+                    </AvatarGroup>
+                </div>
+            </div>
+            <div  className="div-todo">
+                <button className='btn-next'>
+                    Next
+                </button>
+                <button onClick={onExitClick} className='btn-exit'>
+                    Cancel
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default AddClub
--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,8 +1,65 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
+import AddClub from './AddClub';
+import { styled, Box } from "@mui/system";
+import ModalUnstyled from "@mui/core/ModalUnstyled";
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 import "./Home.css";
-const Home = () => {
+const options = [
+  'Ẩn',
+  'Xóa',
+];
+const ITEM_HEIGHT = 48;
+
+
+const StyledModal = styled(ModalUnstyled)`
+  position: fixed;
+  z-index: 1300;
+  right: 0;
+  bottom: 0;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Backdrop = styled("div")`
+  z-index: -1;
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  -webkit-tap-highlight-color: transparent;
+`;
+const Home = (props) => {
+  const [showFormAddClub, setShowFormAddClub] = useState(false);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <div>
+      <StyledModal
+        aria-labelledby="unstyled-modal-title"
+        aria-describedby="unstyled-modal-description"
+        open={showFormAddClub}
+        onClose={() => {
+          setShowFormAddClub(false);
+        }}
+        BackdropComponent={Backdrop}
+      >
+        <AddClub setShowFormAddClub={setShowFormAddClub}/>
+      </StyledModal>
+       
       <div className='div-header'>
         <div className='div-search'>
           <input
@@ -11,14 +68,99 @@ const Home = () => {
 
             />
           <i class="fa-solid fa-magnifying-glass"></i>
+          
         </div>
       </div>
       
       <div className='div-body'>
-        <div className='header-title'> Câu lạc bộ của bạn</div>
+        <div className='header-body'>
+          <div className='header-title'> Câu lạc bộ của bạn</div>
+          <div className='div-btnadd'>
+            <button onClick={()=> setShowFormAddClub(true)} className='btnAdd' >Tạo câu lạc bộ</button>
+            <i class="fa-solid fa-plus"></i>
+          </div>
+        </div>
         <div className='div-card-team'>
             <div className='card-team'>
-            <i className="fa-solid fa-ellipsis"></i>
+            <div className='div-menu'>
+                <IconButton 
+                  aria-label="more"
+                  id="long-button"
+                  aria-controls={open ? 'long-menu' : undefined}
+                  aria-expanded={open ? 'true' : undefined}
+                  aria-haspopup="true"
+                  onClick={handleClick}
+                >
+                  <MoreVertIcon className='icon-menu'/>
+                </IconButton>
+                <Menu
+                  id="long-menu"
+                  MenuListProps={{
+                    'aria-labelledby': 'long-button',
+                  }}
+                  anchorEl={anchorEl}
+                  open={open}
+                  onClose={handleClose}
+                  PaperProps={{
+                    style: {
+                      maxHeight: ITEM_HEIGHT * 4.5,
+                      width: '15ch',
+                    },
+                  }}
+                >
+                  {options.map((option) => (
+                    <MenuItem key={option} selected={option === 'Ẩn'} onClick={handleClose}>
+                      {option}
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </div>
+              <div className='image-team'>
+
+              </div>
+              <div className='name-team'>
+                  CLB Chạy bộ
+              </div>
+              <div className='div-activity'>
+
+              </div>
+              
+            </div>
+            <div className='card-team'>
+            {/* <i className="fa-solid fa-ellipsis"></i> */}
+            <div className='div-menu'>
+                <IconButton 
+                  aria-label="more"
+                  id="long-button"
+                  aria-controls={open ? 'long-menu' : undefined}
+                  aria-expanded={open ? 'true' : undefined}
+                  aria-haspopup="true"
+                  onClick={handleClick}
+                >
+                  <MoreVertIcon className='icon-menu'/>
+                </IconButton>
+                <Menu
+                  id="long-menu"
+                  MenuListProps={{
+                    'aria-labelledby': 'long-button',
+                  }}
+                  anchorEl={anchorEl}
+                  open={open}
+                  onClose={handleClose}
+                  PaperProps={{
+                    style: {
+                      maxHeight: ITEM_HEIGHT * 4.5,
+                      width: '15ch',
+                    },
+                  }}
+                >
+                  {options.map((option) => (
+                    <MenuItem key={option} selected={option === 'Ẩn'} onClick={handleClose}>
+                      {option}
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </div>
               <div className='image-team'>
 
               </div>
