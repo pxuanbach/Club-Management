f68074e1605e3b1da76702b1f9823b2d2ebf7df7
tiendat19521345 <dat31122001@gmail.com>
2022-03-21 07:39:40 +0700

    calendar

--- client/src/components/leader-club/Calendar.js
+++ client/src/components/leader-club/Calendar.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+// import './Calendar.css';
 
-const Calendar = () => {
+const Calendartab = () => {
+  const [date, setDate] = useState(new Date());
   return (
-    <div>umbala</div>
+    <div className='app'>
+      {/* <h1 className='text-center'>React Calendar</h1>
+      <div className='calendar-container'>
+        <Calendar onChange={setDate} value={date} />
+      </div>
+      <p className='text-center'>
+        <span className='bold'>Selected Date:</span>{' '}
+        {date.toDateString()}
+      </p> */}
+    </div>
+
   )
 }
 
-export default Calendar
+export default Calendartab
