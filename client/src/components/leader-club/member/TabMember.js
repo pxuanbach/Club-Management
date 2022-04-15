f020f6ce72d57b06f92fc3f91c4f7556adcf006a
tiendat19521345 <dat31122001@gmail.com>
2022-04-15 22:56:55 +0700

    UI-tabmember

--- client/src/components/leader-club/member/TabMember.js
+++ client/src/components/leader-club/member/TabMember.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Avatar, Divider, Button, Tooltip } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
 import './TabMember.css'
+// import ClearIcon from '@mui/icons-material/Clear';
+// import KeyboardDoubleArrowUpIcon from '@mui/icons-material/KeyboardDoubleArrowUp';
+// import CurrencyExchangeIcon from '@mui/icons-material/CurrencyExchange';
+// import { ENDPT } from '../../../helper/Helper';
+// import io from 'socket.io-client'
+// let socket
 const columns = [
   { field: 'id', headerName: 'STT', width: 70 },
   {
@@ -57,9 +64,32 @@ const rows1 = [
   { id: 4, fullName: 'Nguyễn Văn A', MSSV: '19521345',phoneNumber:'0123456789' },
   { id: 5, fullName: 'Nguyễn Văn A', MSSV: '19521345',phoneNumber:'0123456789' },
 ];
-const TabMember = () => {
+const TabMember = ({ club }) => {
+
   return (
     <div>
+      <div className='tabmember__head'>
+        <div className='members__card'>
+          <h3>Trưởng câu lạc bộ</h3>
+          <div className='member-selected'>
+            <Avatar  />
+            <div className='selected-info'>
+              <span>{'ABC'}</span>
+              <span>{'abc@gmail.com'}</span>
+            </div>
+          </div>
+        </div>
+        <div className='members__card'>
+          <h3>Thủ quỹ</h3>
+          <div className='member-selected'>
+            <Avatar  />
+            <div className='selected-info'>
+              <span>{'ABC'}</span>
+              <span>{'ABC@GMAIL.COM'}</span>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className='div-table-tabmember'>
         <h4 className='name-h4'>Trưởng CLB (2)</h4>
         <div style={{ height: 215, width: '95%',marginTop:'10px',marginLeft:'20px'  }}>
