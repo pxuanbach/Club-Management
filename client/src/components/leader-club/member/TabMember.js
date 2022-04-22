2b83f5d76f382e9e6b319d4926493dac39b36898
pxuanbach <pxuanbach1412@gmail.com>
2022-04-22 10:55:40 +0700

    Update TabMember.js

--- client/src/components/leader-club/member/TabMember.js
+++ client/src/components/leader-club/member/TabMember.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react'
-import { Avatar, Divider, Button, Tooltip } from '@mui/material';
+import { Avatar, Box, Button, Tooltip, TextField } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+import ClearIcon from '@mui/icons-material/Clear';
 import { styled } from '@mui/material/styles';
 import './TabMember.css'
 const CustomTextField = styled(TextField)({
@@ -25,48 +24,61 @@ const style = {
   p: 4,
 };
 
-const columns = [
-  { field: 'id', headerName: 'ID', width: 70,flex:0.5 },
-  { field: 'img_url',
-  headerName: 'Hình đại diện',
-  disableColumnMenu: true,
-  sortable: false,
-  align: 'center',
-  flex: 0.6,
-  renderCell: (value) => {
-    return (
-      <Avatar src={value.row.img_url} />
-    )
-  } },
-  {
-    field: 'fullName', 
-    headerName: 'Họ và tên',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 200,
-    flex:1
-  },
-
-  {
-    field: 'MSSV',
-    headerName: 'MSSV',
-    width: 150,
-    flex:0.7
-    
-  },
-
-  { field: 'phoneNumber', headerName: 'Số điện thoại', width: 150,flex:1 },
-  { field: 'email', headerName: 'Email', width: 150,flex:1.5}
-];
-
-const rows = [
-  { id: 1, fullName: 'Nguyễn Văn A',avata:'', MSSV: '19521345',phoneNumber:'0123456789',email:'19521234@gm.uit.edu.vn' },
-  { id: 2, fullName: 'Nguyễn Văn A', MSSV: '19521345',phoneNumber:'0123456789',email:'19521234@gm.uit.edu.vn' },
-  { id: 3, fullName: 'Nguyễn Văn A', MSSV: '19521345',phoneNumber:'0123456789',email:'19521234@gm.uit.edu.vn' },
-  { id: 4, fullName: 'Nguyễn Văn A', MSSV: '19521345',phoneNumber:'0123456789',email:'19521234@gm.uit.edu.vn' },
-  { id: 5, fullName: 'Nguyễn Văn A', MSSV: '19521345',phoneNumber:'0123456789',email:'19521234@gm.uit.edu.vn' },
-];
 const TabMember = () => {
+
+  const handleRemoveFromClub = (event, param) => {
+    event.stopPropagation();
+    //socket.emit('remove-user-from-club', club._id, param._id)
+  }
+
+  const columns = [
+    { field: '_id', headerName: 'ID', width: 70,flex:0.5 },
+    { field: 'img_url',
+    headerName: 'Hình đại diện',
+    disableColumnMenu: true,
+    sortable: false,
+    align: 'center',
+    flex: 0.6,
+    renderCell: (value) => {
+      return (
+        <Avatar src={value.row.img_url} />
+      )
+    } },
+    {
+      field: 'name', 
+      headerName: 'Họ và tên',
+      description: 'This column has a value getter and is not sortable.',
+      sortable: false,
+      width: 200,
+      flex:1
+    },
+  
+    {
+      field: 'username',
+      headerName: 'Mã sinh viên',
+      flex:0.7
+    },
+    { field: 'email', headerName: 'Email', flex: 1.5},
+    {
+      field: 'btn-remove',
+      headerName: '',
+      align: 'center',
+      flex: 0.4,
+      disableColumnMenu: true,
+      sortable: false,
+      renderCell: (value) => {
+        return (
+          <Tooltip title="Xóa khỏi câu lạc bộ" placement="right-start">
+            <Button style={{ color: '#1B264D' }} disableElevation onClick={(event) => {
+              handleRemoveFromClub(event, value.row)
+            }}>
+              <ClearIcon />
+            </Button>
+          </Tooltip>
+        )
+      }
+    },
+  ];
 
   return (
     <div className='div-tabmember'>
@@ -120,11 +132,10 @@ const TabMember = () => {
         </div>
         <div style={{ height: 400, width: '95%',marginTop:'10px',marginLeft:'20px'  }}>
           <DataGrid
-            rows={rows}
+            rows={null}
             columns={columns}
             pageSize={5}
             rowsPerPageOptions={[5]}
-            checkboxSelection
           />
         </div>
 
