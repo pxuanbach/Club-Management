7ad7d7373d66ebeeff0afd40760a86d977e23c6a
tiendat19521345 <dat31122001@gmail.com>
2022-05-27 00:24:50 +0700

    map data (usercontext) info

--- client/src/components/info/Info.js
+++ client/src/components/info/Info.js
@@ -1,22 +1,24 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import Button from '@mui/material/Button';
 import './Info.css'
 import KeyIcon from '@mui/icons-material/Key';
+import Avatar from '@mui/material/Avatar';
 // import ImageInfo from '../../assets/imageInfo.png'
 import ImageInfo from '../../assets/logoweb.png'
+import {UserContext} from '../../UserContext'
 const Info = () => {
+  const { user, setUser } = useContext(UserContext);
+
   const showhideFunction = () => {
     var actionList = document.getElementById("listButton");    
     if (actionList.className === "list-action-profile")    
     {    
       actionList.className = "display-list-button";  
       document.getElementById("textinput").style.display = "block";
-      document.getElementById("textinput1").style.display = "block";
       document.getElementById("textinput2").style.display = "block";
       document.getElementById("textinput3").style.display = "block";
       document.getElementById("textinput4").style.display = "block";
       document.getElementById("textresult").style.display = "none";
-      document.getElementById("textresult1").style.display = "none";
       document.getElementById("textresult2").style.display = "none";
       document.getElementById("textresult3").style.display = "none";
       document.getElementById("textresult4").style.display = "none";
@@ -24,12 +26,10 @@ const Info = () => {
     {      
       actionList.className = "list-action-profile";  
       document.getElementById("textinput").style.display = "none";
-      document.getElementById("textinput1").style.display = "none";
       document.getElementById("textinput2").style.display = "none";
       document.getElementById("textinput3").style.display = "none";
       document.getElementById("textinput4").style.display = "none";
       document.getElementById("textresult").style.display = "block";
-      document.getElementById("textresult1").style.display = "block";
       document.getElementById("textresult2").style.display = "block";
       document.getElementById("textresult3").style.display = "block";
       document.getElementById("textresult4").style.display = "block";
@@ -73,42 +73,46 @@ const showhideFunction2 = () => {
             <h4 className='title-profile-1'>Thông tin chung</h4>
             <h5 onClick={() => showhideFunction() }>Edit</h5>
           </div>
+          
           <div style={{display:"flex", paddingTop:10}}>
             <div className='div-action-image'>
-              <div className='image'></div>
+              <div className='image'>
+                <Avatar sx={{width: 120, height: 120}} src={user.img_url}/>
+              </div>
               <Button variant='text' sx={{textTransform: "none",}}>Xóa ảnh</Button>
             </div>
             <div style={{flex:1, paddingLeft:40}}>
               <div className='div-text-profile'>
                 <label>Họ và tên:</label>
-                <p id='textresult'>Nguyễn Tiến Đạt</p>
-                <input id='textinput' className='input'></input>
+                <p id='textresult'>{user.name}</p>
+                <input id='textinput' className='input' placeholder={user.name}></input>
               </div>
-              <div className='div-text-profile'>
+              {/* <div className='div-text-profile'>
                 <label>Ngày sinh:</label>
                 <p id='textresult1'>31/12/2001</p>
                 <input id='textinput1' className='input'></input>
-              </div>
+              </div> */}
               <div className='div-text-profile'>
                 <label>Giới tính:</label>
-                <p id='textresult2'>Nam</p>
-                <select id='textinput2' className='input'>
+                <p id='textresult2'>{user.gender}</p>
+                <select id='textinput2' className='input'  placeholder={user.gender}>
                   <option>Nam</option>
                   <option>Nữ</option>
                 </select>
               </div>
               <div className='div-text-profile'>
                 <label>Số điện thoại:</label>
-                <p id='textresult3'>0123456789</p>
-                <input id='textinput3' className='input'></input>
+                <p id='textresult3'>{user.phone}</p>
+                <input id='textinput3' className='input'  placeholder={user.phone}></input>
               </div>
               <div className='div-text-profile'>
                 <label>Địa chỉ email:</label>
-                <p id='textresult4'>datdeptrai@gmail.com</p>
-                <input id='textinput4' className='input'></input>
+                <p id='textresult4'>{user.email}</p>
+                <input id='textinput4' className='input'  placeholder={user.email}></input>
               </div>
             </div>
           </div>
+  
           <div id='listButton' className='list-action-profile'>
             <Button variant='outlined' onClick={() => showhideFunction() }>Hủy</Button>
             <Button variant='outlined'>Lưu</Button>
@@ -120,8 +124,8 @@ const showhideFunction2 = () => {
             <h5 onClick={() => showhideFunction1() } >Edit</h5>
           </div>
           <div className='div-text-profile'>
-            <p id='textresult5' className='text-description-profile'>Tôi là một con người trầm tính. Đam mê và nhiệt huyết với công việc.aaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-            <textarea id="textinput5" className='textarea-description'></textarea>
+            <p id='textresult5' className='text-description-profile'>{user.description}</p>
+            <textarea id="textinput5" className='textarea-description'  placeholder={user.description}></textarea>
           </div>
           <div id='listButton1' className='list-action-profile'>
             <Button variant='outlined' onClick={() => showhideFunction1() }>Hủy</Button>
@@ -135,8 +139,8 @@ const showhideFunction2 = () => {
           </div>
           <div className='div-text-profile'>
             <label>Facebook:</label>
-            <p id='textresult6'>https://www.facebook.com/</p>
-            <input id="textinput6"></input>
+            <p id='textresult6'>{user.facebook}</p>
+            <input id="textinput6"  placeholder={user.facebook}></input>
           </div>
           <div id='listButton2' className='list-action-profile'>
             <Button variant='outlined' onClick={() => showhideFunction2() }>Hủy</Button>
