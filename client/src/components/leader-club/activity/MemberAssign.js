fb13ce0a9312bc6d132b4072a14dca8380866b92
tiendat19521345 <dat31122001@gmail.com>
2022-05-27 17:34:06 +0700

    UI-CardDetail-Update

--- client/src/components/leader-club/activity/MemberAssign.js
+++ client/src/components/leader-club/activity/MemberAssign.js
@@ -1,12 +1,21 @@
 import React, { useState, useEffect, useRef } from 'react'
 import './MemberAssign.css'
 
-const MemberAssgin = () => {
+const MemberAssgin = ({setShow}) => {
     return(
         <div className='container-memberassign'>
             <div className='header-memberassign'>
-                <h3>Thành viên</h3>
-                <i class="fa-solid fa-xmark"></i>
+                <i class="fa-solid fa-xmark" onClick={() => setShow(false)}></i>
+                <h3 className='title-header-memberassign'>Thành viên</h3> 
+            </div>
+            <div>
+                <div className='div-search-memberassign'>
+                    <input 
+                        type="text"
+                        placeholder="Tìm kiếm trên Message"
+                    />
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </div>
             </div>
         </div>
     )
