fc913faedfce36de0de0738963addb09296f1eb9
tiendat19521345 <dat31122001@gmail.com>
2022-10-24 22:40:58 +0700

    UI-Tab-Manage-Invite

--- client/src/components/leader-club/invite/Invite.js
+++ client/src/components/leader-club/invite/Invite.js
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import './Invite.css'
+import SendInvite from './Send-Invite'
+import NavTabs from './Tab-Invite'
 const Invite = () => {
     return(
-        <div>
-            abc
+        <div className='div-send-invite'>
+            <div className='container-send-invite'>
+                <h2 className='title-noti'>Quản lý lời mời</h2>
+                <NavTabs/>
+            </div>
         </div>
     )
 
